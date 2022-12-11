30081532e23b6697470b0dbad86e09c4f8c0731a
Khaled <khalegjr@gmail.com>
2022-12-11 16:31:08 -0300

    Convertendo para um formato que possa ser enviado para a API

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from "src/app/interfaces/Cliente";
 
 @Component({
   selector: 'app-clientes',
@@ -13,8 +14,24 @@ export class ClientesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  createHandler(event:any): void {
-    console.warn('FOIII')
+  async createHandler(cliente: Cliente) {
+    const formData = new FormData()
+
+    formData.append("nome", cliente.nome)
+    formData.append("cpf", cliente.cpf)
+    formData.append("logradouro", cliente.logradouro)
+    formData.append("numero", cliente.numero)
+    formData.append("complemento", cliente.complemento)
+    formData.append("bairro", cliente.bairro)
+    formData.append("cidade", cliente.cidade)
+    formData.append("estado", cliente.estado)
+    formData.append("cep", cliente.cep)
+    formData.append("email", cliente.email)
+    formData.append("data_nascimento", cliente.data_nascimento)
+
+    // TODO: enviar para a API
+    // TODO: exibir mensagem de sucesso ou erro
+    // TODO: redirect
   }
 
 }
