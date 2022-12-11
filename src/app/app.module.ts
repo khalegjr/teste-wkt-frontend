cc584d9f93e7dc457e7d302028ab565994dd80c3
Khaled <khalegjr@gmail.com>
2022-12-11 12:46:01 -0300

    Cria componentes de páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ClientesComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/clientes/clientes.component.html
@@ -0,0 +1,1 @@
+<h1>Listagem de clientes</h1>
