ea9ecca887768487fc8c1dca60a45bcbc843425e
Khaled <khalegjr@gmail.com>
2022-12-11 12:46:24 -0300

    Cria rotas para as páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { HomeComponent } from "./pages/home/home.component";
+import { ProdutosComponent } from './pages/produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: '',
+  },
+  {
+    component: ClientesComponent,
+    path: 'clientes',
+  },
+  {
+    component: ProdutosComponent,
+    path: 'produtos'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
