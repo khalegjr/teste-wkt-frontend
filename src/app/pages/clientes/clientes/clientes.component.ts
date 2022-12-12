3bfb9b011d53d4aa38495291fc56b08c67db2582
Khaled <khalegjr@gmail.com>
2022-12-12 20:31:55 -0300

    lint: Linter de código

--- src/app/pages/clientes/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes/clientes.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { ClientesService } from 'src/app/services/clientes.service';
+import { ClientesService } from "src/app/services/clientes.service";
 import { environment } from "src/environments/environment";
+import { MessagesService } from "./../../../services/messages.service";
 
 @Component({
-  selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.scss']
+  selector: "app-clientes",
+  templateUrl: "./clientes.component.html",
+  styleUrls: ["./clientes.component.scss"],
 })
 export class ClientesComponent implements OnInit {
-  allClientes: any = []
-  clientes: any = []
-  baseApiUrl = environment.baseApiUrl
+  allClientes: any = [];
+  clientes: any = [];
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor(private clientesService: ClientesService, private route: ActivatedRoute) { }
+  constructor(
+    private clientesService: ClientesService,
+    private route: ActivatedRoute,
+    private message: MessagesService
+  ) {}
 
   ngOnInit(): void {
     this.clientesService.getClientes().subscribe((items) => {
-
-      this.clientes = items
-    })
+      this.clientes = items;
+      console.log(items);
+    });
   }
 
+  async removeHandler(id: number) {
+    await this.clientesService.removeCliente(id).subscribe();
+
+    this.message.add("Cliente excluído com sucesso", "/clientes");
+  }
 }
--- src/app/pages/clientes/form-cliente/form-cliente.component.ts
+++ src/app/pages/clientes/form-cliente/form-cliente.component.ts
@@ -1,49 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { Cliente } from "src/app/interfaces/Cliente";
 import { ClientesService } from "src/app/services/clientes.service";
 import { MessagesService } from "src/app/services/messages.service";
 
 @Component({
-  selector: 'app-form-cliente',
-  templateUrl: './form-cliente.component.html',
-  styleUrls: ['./form-cliente.component.scss']
+  selector: "app-form-cliente",
+  templateUrl: "./form-cliente.component.html",
+  styleUrls: ["./form-cliente.component.scss"],
 })
 export class FormClienteComponent implements OnInit {
-  btnText = 'Cadastrar'
+  btnText = "Cadastrar";
 
   constructor(
     private clientesService: ClientesService,
     private messageService: MessagesService,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   async createHandler(cliente: Cliente) {
-    const formData = new FormData()
+    const formData = new FormData();
 
-    formData.append("nome", cliente.nome)
-    formData.append("cpf", cliente.cpf)
-    formData.append("logradouro", cliente.logradouro)
-    formData.append("numero", cliente.numero)
-    formData.append("bairro", cliente.bairro)
-    formData.append("cidade", cliente.cidade)
-    formData.append("estado", cliente.estado)
-    formData.append("cep", cliente.cep)
-    formData.append("email", cliente.email)
-    formData.append("data_nascimento", cliente.data_nascimento)
+    formData.append("nome", cliente.nome);
+    formData.append("cpf", cliente.cpf);
+    formData.append("logradouro", cliente.logradouro);
+    formData.append("numero", cliente.numero);
+    formData.append("bairro", cliente.bairro);
+    formData.append("cidade", cliente.cidade);
+    formData.append("estado", cliente.estado);
+    formData.append("cep", cliente.cep);
+    formData.append("email", cliente.email);
+    formData.append("data_nascimento", cliente.data_nascimento);
 
     if (cliente.complemento) {
-      formData.append("complemento", cliente.complemento)
+      formData.append("complemento", cliente.complemento);
     }
 
-    this.clientesService.criaCliente(formData).subscribe()
+    this.clientesService.criaCliente(formData).subscribe();
 
-    this.messageService.add('Cliente adicionado com sucesso')
-
-    this.router.navigate(['/'])
+    this.messageService.add("Cliente adicionado com sucesso", "clientes");
   }
-
 }
