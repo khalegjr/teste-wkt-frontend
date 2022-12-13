6f50f16b64b95fe33b0b40a5a99c7f4ee18c04a4
Khaled <khalegjr@gmail.com>
2022-12-13 03:34:44 -0300

    refac: Altera formulário de cliente para servir para edição

--- src/app/componentes/cliente-form/cliente-form.component.ts
+++ src/app/componentes/cliente-form/cliente-form.component.ts
@@ -1,120 +1,161 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Cliente } from "../../interfaces/Cliente";
 
 enum Estados {
-  'AC' = 'Acre',
-  'AL' = 'Alagoas',
-  'AP' = 'Amapá',
-  'AM' = 'Amazonas',
-  'BA' = 'Bahia',
-  'CE' = 'Ceará',
-  'DF' = 'Distrito Federal',
-  'ES' = 'Espírito Santo',
-  'GO' = 'Goiás',
-  'MA' = 'Maranhão',
-  'MT' = 'Mato Grosso',
-  'MS' = 'Mato Grosso do Sul',
-  'MG' = 'Minas Gerais',
-  'PA' = 'Pará',
-  'PB' = 'Paraíba',
-  'PR' = 'Paraná',
-  'PE' = 'Pernambuco',
-  'PI' = 'Piauí',
-  'RJ' = 'Rio de Janeiro',
-  'RN' = 'Rio Grande do Norte',
-  'RS' = 'Rio Grande do Sul',
-  'RO' = 'Rondônia',
-  'RR' = 'Roraima',
-  'SC' = 'Santa Catarina',
-  'SP' = 'São Paulo',
-  'SE' = 'Sergipe',
-  'TO' = 'Tocantins'
+  "AC" = "Acre",
+  "AL" = "Alagoas",
+  "AP" = "Amapá",
+  "AM" = "Amazonas",
+  "BA" = "Bahia",
+  "CE" = "Ceará",
+  "DF" = "Distrito Federal",
+  "ES" = "Espírito Santo",
+  "GO" = "Goiás",
+  "MA" = "Maranhão",
+  "MT" = "Mato Grosso",
+  "MS" = "Mato Grosso do Sul",
+  "MG" = "Minas Gerais",
+  "PA" = "Pará",
+  "PB" = "Paraíba",
+  "PR" = "Paraná",
+  "PE" = "Pernambuco",
+  "PI" = "Piauí",
+  "RJ" = "Rio de Janeiro",
+  "RN" = "Rio Grande do Norte",
+  "RS" = "Rio Grande do Sul",
+  "RO" = "Rondônia",
+  "RR" = "Roraima",
+  "SC" = "Santa Catarina",
+  "SP" = "São Paulo",
+  "SE" = "Sergipe",
+  "TO" = "Tocantins",
 }
 
 @Component({
-  selector: 'app-cliente-form',
-  templateUrl: './cliente-form.component.html',
-  styleUrls: ['./cliente-form.component.scss']
+  selector: "app-cliente-form",
+  templateUrl: "./cliente-form.component.html",
+  styleUrls: ["./cliente-form.component.scss"],
 })
 export class ClienteFormComponent implements OnInit {
-  @Output() onSubmit = new EventEmitter<Cliente>()
-  @Input() btnText!: string
-  lista_estados = Estados
+  @Output() onSubmit = new EventEmitter<Cliente>();
+  @Input() btnText!: string;
+  @Input() tituloForm: string | null = null;
+  @Input() clienteData: any | null = null;
+  lista_estados = Estados;
 
-  clienteForm!: FormGroup
+  clienteForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.clienteForm = new FormGroup(
-      {
-        id: new FormControl(''),
-        nome: new FormControl('', Validators.required),
-        cpf: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(11), Validators.pattern('[0-9]{11}')])),
-        logradouro: new FormControl('', Validators.required),
-        numero: new FormControl('', Validators.required),
-        complemento: new FormControl(''),
-        bairro: new FormControl('', Validators.required),
-        cidade: new FormControl('', Validators.required),
-        estado: new FormControl('', Validators.required),
-        cep: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(11), Validators.pattern('[0-9]{5}-[0-9]{3}')])),
-        email: new FormControl('',Validators.compose([Validators.required, Validators.email])),
-        data_nascimento: new FormControl('', Validators.required),
-      }
-    )
+    console.log("opa " + this.btnText);
+
+    this.clienteForm = new FormGroup({
+      id: new FormControl(this.clienteData ? this.clienteData.id : ""),
+      nome: new FormControl(
+        this.clienteData ? this.clienteData.nome : "",
+        Validators.required
+      ),
+      cpf: new FormControl(
+        this.clienteData ? this.clienteData.cpf : "",
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(11),
+          Validators.pattern("[0-9]{11}"),
+        ])
+      ),
+      logradouro: new FormControl(
+        this.clienteData ? this.clienteData.logradouro : "",
+        Validators.required
+      ),
+      numero: new FormControl(
+        this.clienteData ? this.clienteData.numero : "",
+        Validators.required
+      ),
+      complemento: new FormControl(
+        this.clienteData ? this.clienteData.complemento : ""
+      ),
+      bairro: new FormControl(
+        this.clienteData ? this.clienteData.bairro : "",
+        Validators.required
+      ),
+      cidade: new FormControl(
+        this.clienteData ? this.clienteData.cidade : "",
+        Validators.required
+      ),
+      estado: new FormControl(
+        this.clienteData ? this.clienteData.estado : "",
+        Validators.required
+      ),
+      cep: new FormControl(
+        this.clienteData ? this.clienteData.cep : "",
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(11),
+          Validators.pattern("[0-9]{5}-[0-9]{3}"),
+        ])
+      ),
+      email: new FormControl(
+        this.clienteData ? this.clienteData.email : "",
+        Validators.compose([Validators.required, Validators.email])
+      ),
+      data_nascimento: new FormControl(
+        this.clienteData ? this.clienteData.data_nascimento : "",
+        Validators.required
+      ),
+    });
   }
 
   get nome() {
-    return this.clienteForm.get('nome')!
+    return this.clienteForm.get("nome")!;
   }
 
   get cpf() {
-    return this.clienteForm.get('cpf')!
+    return this.clienteForm.get("cpf")!;
   }
 
   get logradouro() {
-    return this.clienteForm.get('logradouro')!
+    return this.clienteForm.get("logradouro")!;
   }
 
   get numero() {
-    return this.clienteForm.get('numero')!
+    return this.clienteForm.get("numero")!;
   }
 
   get bairro() {
-    return this.clienteForm.get('bairro')!
+    return this.clienteForm.get("bairro")!;
   }
 
-   get complemento() {
-    return this.clienteForm.get('complemento')!
+  get complemento() {
+    return this.clienteForm.get("complemento")!;
   }
 
   get cidade() {
-    return this.clienteForm.get('cidade')!
+    return this.clienteForm.get("cidade")!;
   }
 
   get estado() {
-    return this.clienteForm.get('estado')!
+    return this.clienteForm.get("estado")!;
   }
 
-   get cep() {
-    return this.clienteForm.get('cep')!
+  get cep() {
+    return this.clienteForm.get("cep")!;
   }
 
   get email() {
-    return this.clienteForm.get('email')!
+    return this.clienteForm.get("email")!;
   }
 
   get data_nascimento() {
-    return this.clienteForm.get('data_nascimento')!
+    return this.clienteForm.get("data_nascimento")!;
   }
 
   submit(): void {
     if (this.clienteForm.invalid) {
-      return
+      return;
     }
 
-    this.onSubmit.emit(this.clienteForm.value)
+    this.onSubmit.emit(this.clienteForm.value);
   }
-
 }
